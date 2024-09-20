c108fece473798c0824da77a56ec849777f58cd2
maxwel kirimi <maxwelkirimii@gmail.com>
2024-09-20 11:02:30 +0300

    document structure

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`;
+}
+
+function introductionWithLanguage(Aki, JavaScript) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`;
+}
+function introductionWithLanguageOptional(Aki, JavaScript = 'JavaScript') {
+    return `Hi, my name is ${Aki} and I am learning to program in ${JavaScript}.`;
+} 
